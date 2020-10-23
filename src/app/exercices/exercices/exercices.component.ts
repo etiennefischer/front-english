abcdce38de35026752c001cfe7f16e3736d13686
kouzty <hanss.bruno@gmail.com>
2020-10-23 13:33:40 +0200

    info-msg working properly

--- src/app/exercices/exercices/exercices.component.ts
+++ src/app/exercices/exercices/exercices.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {ExerciceService} from './exercice';
 import { CHAPTER } from './../../../mock-chapter';
 import { InfoMsgService} from '../../_shared/info-msg/info-msg.service';
+import {delay} from 'rxjs/operators';
 
 interface Answer {
   msg: InfoMsgService;
@@ -14,20 +15,18 @@ interface Answer {
 })
 export class ExercicesComponent implements OnInit {
   exercices = CHAPTER;
-
   currentExercice: number;
   selectedReponse: string;
+  correctionText: string;
   structure: any[];
 
   constructor(private msg: InfoMsgService) {
     this.currentExercice = 0;
 
-
   }
 
   ngOnInit(): void {
-    console.log(this.structure);
-    console.log(this.currentExercice);
+
   }
 
   onSelect(reponse: string): void {
@@ -36,20 +35,19 @@ export class ExercicesComponent implements OnInit {
     this.isExerciceDone();
     this.structure = this.breadcrumbsArray(this.currentExercice);
 
-
-
-
-
   }
   checkAnswer(): boolean {
 
     if (this.selectedReponse === this.exercices[this.currentExercice].answer){
       this.currentExercice += 1;
       this.msg.clear();
-      this.msg.add('Bonne réponse !', '0');
+      this.msg.add("Bonne réponse !", "0");
+      this.resetCorrectionMsg();
     }else {
       this.msg.clear();
-      this.msg.add('Mauvaise réponse !', '1');
+      this.msg.add("Mauvaise réponse !", "1");
+      this.addCorrectionText();
+
     }
 
     return true;
@@ -57,7 +55,6 @@ export class ExercicesComponent implements OnInit {
   // tslint:disable-next-line:typedef
   isExerciceDone() {
     if (this.exercices.length <= this.currentExercice) {
-      console.log('Exercice terminé !');
       this.currentExercice = 0;
     }
   }
@@ -68,4 +65,12 @@ export class ExercicesComponent implements OnInit {
     }
     return array;
   }
+  addCorrectionText(): any {
+    this.correctionText = this.exercices[this.currentExercice].correction;
+
+  }
+  resetCorrectionMsg(): void {
+    this.correctionText = '';
+  }
+
 }
